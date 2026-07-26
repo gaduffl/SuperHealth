69fe92e7f7e5b1736b2e29970ca290de7f226dc2
gaduffl <100528925+gaduffl@users.noreply.github.com>
2026-07-26 18:27:35 +0200

    Complete strict analyzer cleanup

--- test/data/profile_height_test.dart
+++ test/data/profile_height_test.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:super_health/data/app_database.dart';
 import 'package:super_health/data/health_repository.dart';
