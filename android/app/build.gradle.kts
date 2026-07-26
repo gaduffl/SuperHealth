5cb9c506a304f4297139ac166c51e95989634b93
gaduffl <100528925+gaduffl@users.noreply.github.com>
2026-07-26 17:16:13 +0200

    Sync final SuperHealth source [skip ci]

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,10 +15,12 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
         applicationId = "com.gaduffl.super_health"
+        multiDexEnabled = true
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -48,6 +50,10 @@ android {
             }
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 kotlin {
